bc4aff74358b3e4f89367a6aae2db123f17f49db
Mariana <marianamoskalyk526@gmail.com>
2025-11-28 23:10:08 +0200

    Фінальна фіксація індексу та всіх файлів dz8

---
+++ dz8/src/App.jsx
@@ -0,0 +1,16 @@
+function MyCard() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+      }}
+    >
+      <h2>Мій компонент 💙</h2>
+      <p>Це працює!</p>
+    </div>
+  );
+}
+
+export default MyCard;
